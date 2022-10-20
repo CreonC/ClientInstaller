fd19df0ba92adca5aca33a4ff71c2e92fd67766e
CreonC <69414582+CreonC@users.noreply.github.com>
2022-10-20 17:21:55 +0800

    Update install.bat
    Make it more readable for human eyes

--- install.bat
+++ install.bat
@@ -1,12 +1,14 @@
 @echo off
-echo installing CreonC Mod Client
+echo Installing Mod Client
+rem Function PROMPT, Main func to install the client
 :PROMPT
-SET /P AREYOUSURE=are you sure to continue? this will destoy all data inside your mods folder. if you are unsure, hit NO. if continue, hit YES
+SET /P AREYOUSURE=are you sure to continue? this will delete all data inside your mods folder. if you are unsure, hit NO. if continue, hit YES
 IF /I "%AREYOUSURE%" NEQ "YES" GOTO END
-echo client-helper- sit back and relax while installer is helping you to install CreonC Mod Client 
 echo killing minecraft launcher
 taskkill /im MinecraftLauncher.exe /f
-ECHO installer-client-1- getting files
+rem Killing the minecraft launcher
+ECHO getting files
+rem Copy the installer.zip into .minecraft, then unzip the installer.zip and paste the mod folder (if installer.zip really exists)
 copy install.zip C:\Users\%username%\appdata\roaming\.minecraft\
 cd C:\Users\%username%\appdata\roaming\.minecraft\
 del install
@@ -18,13 +20,15 @@ copy C:\Users\%username%\appdata\roaming\.minecraft\install\mods C:\Users\%usern
 echo copying mods complete
 echo installing client
 cd C:\Users\%username%\appdata\roaming\.minecraft\versions
-md 1.17-CreonC-Mod-Client
+rem feel free to remame the line below
+md 1.19.2-CreonC-Mod-Client
 copy C:\Users\%username%\appdata\roaming\.minecraft\install\mcinstall\1.17-CreonC-Mod-Client C:\Users\%username%\appdata\roaming\.minecraft\versions\1.17-CreonC-Mod-Client
+rem clear the screen
 cls
 echo done!
 echo please restart minecraft launcher 
 echo and click on new installation
-echo and select 1.17-CreonC-Mod-Client
+echo and select 1.19.2-CreonC-Mod-Client
 echo make sure you have modded in version on
 pause 5
 exit
